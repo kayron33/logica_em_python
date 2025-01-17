911da119f588587f7827a6950ac1daf8b23d04f0
kayron33 <129231712+kayron33@users.noreply.github.com>
2025-01-16 22:38:48 -0300

    atualização ultilizando o kivy

--- treinando o kivy/Codigo 01.py
+++ treinando o kivy/Codigo 01.py
@@ -2,15 +2,17 @@
 #criar o aplicativo
 #criar a função build do aplicativo
 from kivy.app import App
-from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
-
-GUI = Builder.load_file("front-end.kv")
-
-
-class MeuAplicativo(App):
+class Tarefas(BoxLayout):
+    def __init__(self,tarefas,**kwargs):
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.add_widget(Label(text = tarefa, font_size = 30))
+        
+class Teste(App):
     def build(self):
-        return GUI
-    
-MeuAplicativo().run()
-    
+        return Tarefas(["Fazer compras", "Buscar filho","Molhar a calçada"] , orientation = 'horizontal')
+   
+Teste().run()
