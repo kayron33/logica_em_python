274bf94858934eda7b74754de198f7ae848b5693
kayron33 <129231712+kayron33@users.noreply.github.com>
2025-01-18 09:43:10 -0300

    Atualização do kiv

--- treinando o kivy/Codigo 01.py
+++ treinando o kivy/Codigo 01.py
@@ -4,15 +4,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
-class Tarefas(BoxLayout):
+class Tarefas(ScrollView):
     def __init__(self,tarefas,**kwargs):
         super().__init__(**kwargs)
         for tarefa in tarefas:
-            self.add_widget(Label(text = tarefa, font_size = 30))
-        
+            self.ids.box.add_widget(Tarefa(text = tarefa))
+class Tarefa(BoxLayout):
+    def __init__(self,text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
 class Teste(App):
     def build(self):
-        return Tarefas(["Fazer compras", "Buscar filho","Molhar a calçada"] , orientation = 'horizontal')
+        return Tarefas(["Fazer compras", "Buscar filho","Molhar a calçada","12321313","12312312","123123"] )
    
 Teste().run()
